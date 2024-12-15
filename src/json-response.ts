33ed946c93cd4f851fd7a82223e968e5cc2c74e6
RonY <sunnyrawat225@gmail.com>
2024-12-15 19:54:37 +0530

    Implemented default Logo things

--- src/json-response.ts
+++ src/json-response.ts
@@ -1,4 +1,5 @@
 import { getErrorMessage } from './get-error-message'
+import { prettySiteName } from './utils'
 
 const generateJSONResponse = (obj: any) => {
   return new Response(JSON.stringify(obj), {
@@ -11,10 +12,27 @@ const generateJSONResponse = (obj: any) => {
 
 const generateErrorJSONResponse = (error: unknown, url?: string) => {
   const errorMessage = getErrorMessage(error)
+  if(!url){
+    //If we don't have a URL let's return the error message
+    return generateJSONResponse({
+      error: errorMessage,
+      url,
+    })
+}
+
+  const pretty_site_name = prettySiteName(url) 
+  const errorResp={
+    favicon:[`https://logo.clearbit.com/${pretty_site_name.toLocaleLowerCase()}`],
+    title:pretty_site_name,
+    url,
+    image:[`https://logo.clearbit.com/${pretty_site_name.toLocaleLowerCase()}`],
+  }
   return generateJSONResponse({
     error: errorMessage,
-    url,
+    ...errorResp
   })
+  //If we have an error let's return better Data so the consumer don't have to deal with default shit
+
 }
 
 export { generateJSONResponse, generateErrorJSONResponse }
